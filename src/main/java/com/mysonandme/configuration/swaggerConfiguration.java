bafd2e7177117a0ce0a5b2c4c08879fa987e12d9
Jyf010205 <jianyufeng@agree.com.cn>
2019-11-02 23:02:37 +0800

    新建项目

---
+++ src/main/java/com/mysonandme/configuration/swaggerConfiguration.java
@@ -0,0 +1,35 @@
+package com.mysonandme.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author: jianyufeng
+ * @description: Swagger配置类
+ * Swagger访问地址：http://localhost:8080/swagger-ui.html#
+ */
+
+@Configuration
+public class swaggerConfiguration {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mysonandme.facechina.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInfo(){
+        return new ApiInfoBuilder()
+                .title("FaceChina接口系统")
+                .version("1.0").build();
+    }
+}
